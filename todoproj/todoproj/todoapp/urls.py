226e8a5b9e49e1a7d964c7b45077a53064994603
Binshabs <binshabs567@gmail.com>
2024-01-09 17:38:03 +0530

    file added

--- todoproj/todoproj/todoapp/urls.py
+++ todoproj/todoproj/todoapp/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('',views.Home,name='Home'),
     path('update/<int:id>',views.Update,name='update'),
     
+    path('delete/<int:id>',views.Delete,name='delete'),
+    
     
     
     # path('contact/',views.contact,name='contact')
--- todoproj/todoproj/todoapp/views.py
+++ todoproj/todoproj/todoapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
+
 
 # Create your views here.
 
@@ -8,23 +9,49 @@ def Home(req):
     tasks=Task.objects.all()
     if req.method=="POST":
         task=req.POST.get('task','')
-        priority=req.POST.get('prority','')
+        priority=req.POST.get('priority','')
+        print(task,priority)
         todo=Task(task=task,priority=priority)
         todo.save()
-    
     return render(req,'index.html',{"tasks":tasks})
 
 def Update(req,id):
     tasks=Task.objects.get(id=id)
     if req.method=="POST":
-     task=req.POST.get('task','')
-     priority=req.POST.get('prority','')
-    
-    # print(id)
-    Task.objects.get(id=id)
-    return render(req,'update.html',{"task":task})
+       task=req.POST.get('task','')
+       priority=req.POST.get('priority','')
+       
+       Task.objects.filter(id=id).update(task=task,priority=priority)
+       return redirect("home")
+    return render(req,'update.html',{"task":tasks})
 
     
+
+def Delete(req,id):
+    tasks=Task.objects.get(id=id)
+    if req.method=="POST":
+    #    task=req.POST.get('task','')
+    #    priority=req.POST.get('prority','')
+       Task.objects.filter(id=id).delete()
+       
+       return redirect("home")
+    return render(req,'delete.html',{"task":tasks})
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # def Home(req):
