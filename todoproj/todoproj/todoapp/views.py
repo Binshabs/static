41ca64b7e9183943ee4790e0ab78356429f65693
Binshabs <binshabs567@gmail.com>
2024-01-11 09:13:54 +0530

    new file

--- todoproj/todoproj/todoapp/views.py
+++ todoproj/todoproj/todoapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Task
-
-
+from .forms import TodoForm
 # Create your views here.
 
 
@@ -14,27 +13,35 @@ def Home(req):
         priority=req.POST.get('priority','')
         date=req.POST.get('date','')
         img=req.FILES['image']
-        # print(task,priority)
+        # print(task,priority,img)
         todo=Task(task=task,priority=priority,date=date,image=img)
         todo.save()
     return render(req,'index.html',{"task":tasks})
 
 def Update(req,id):
     tasks=Task.objects.get(id=id)
-    if req.method=="POST":
-       task=req.POST.get('task','')
-       priority=req.POST.get('priority','')
-       Task.objects.filter(id=id).update(task=task,priority=priority)
-       return redirect("home")
-    return render(req,'update.html',{"task":tasks})
+    # if req.method=="POST":
+    #    task=req.POST.get('task','')
+    #    priority=req.POST.get('priority','')
+    #    Task.objects.filter(id=id).update(task=task,priority=priority)
+    #    return redirect("home")
+    
+    
+    
+    f=TodoForm(req.POST or None,instance=tasks)
+    if f.is_valid():
+        f.save()
+        return redirect("home")
+        
+    return render(req,'formUpdate.html',{"task":tasks,'f':f})
 
     
 
 def Delete(req,id):
     tasks=Task.objects.get(id=id)
     if req.method=="POST":
-    #    task=req.POST.get('task','')
-    #    priority=req.POST.get('prority','')
+       task=req.POST.get('task','')
+       priority=req.POST.get('prority','')
        Task.objects.filter(id=id).delete()
        
        return redirect("home")
